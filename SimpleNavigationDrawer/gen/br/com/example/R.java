157c3bb2b21622a33b28a494d5154ed19e61f8a3
danielso2007 <danielso2007@gmail.com>
2014-10-16 03:15:08 -0300

    xx

--- SimpleNavigationDrawer/gen/br/com/example/R.java
+++ SimpleNavigationDrawer/gen/br/com/example/R.java
@@ -10,6 +10,28 @@ package br.com.example;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int azul_pinngo=0x7f040000;
+        public static final int background=0x7f040009;
+        public static final int background_branco=0x7f040005;
+        public static final int black=0x7f040008;
+        public static final int cinza_claro=0x7f040003;
+        public static final int cinza_escuro=0x7f040004;
+        /**  List tela Principal 
+         */
+        public static final int genre=0x7f04000b;
+        public static final int gray=0x7f040007;
+        public static final int list_divider=0x7f04000d;
+        public static final int list_row_end_color=0x7f04000f;
+        public static final int list_row_hover_end_color=0x7f040011;
+        public static final int list_row_hover_start_color=0x7f040010;
+        public static final int list_row_start_color=0x7f04000e;
+        public static final int roxo_pinngo=0x7f040001;
+        public static final int texto_branco=0x7f040002;
+        public static final int transparent=0x7f04000a;
+        public static final int white=0x7f040006;
+        public static final int year=0x7f04000c;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,59 +40,90 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int genre=0x7f050009;
+        public static final int login_horizontal_margin=0x7f050003;
+        public static final int login_horizontal_margin_land=0x7f050005;
+        public static final int login_vertical_margin=0x7f050004;
+        public static final int login_vertical_margin_land=0x7f050006;
         /** 
          Per the design guidelines, navigation drawers should be between 240dp and 320dp:
          https://developer.android.com/design/patterns/navigation-drawer.html
     
          */
-        public static final int navigation_drawer_width=0x7f040002;
+        public static final int navigation_drawer_width=0x7f050002;
+        public static final int rating=0x7f050008;
+        /**  List Tela Principal 
+         */
+        public static final int title=0x7f050007;
+        public static final int year=0x7f05000a;
     }
     public static final class drawable {
         public static final int actionbar_background=0x7f020000;
         public static final int drawer_shadow=0x7f020001;
-        public static final int ic_drawer=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
+        public static final int gas_icon=0x7f020002;
+        public static final int gas_icon_check=0x7f020003;
+        public static final int gas_icon_error=0x7f020004;
+        public static final int ic_drawer=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int ic_pin=0x7f020007;
+        public static final int ic_principal=0x7f020008;
+        public static final int ic_system_preferences=0x7f020009;
+        public static final int list_row_bg=0x7f02000a;
+        public static final int list_row_bg_hover=0x7f02000b;
+        public static final int list_row_selector=0x7f02000c;
+        public static final int oil_icon=0x7f02000d;
+        public static final int oil_icon_check=0x7f02000e;
+        public static final int oil_icon_error=0x7f02000f;
     }
     public static final class id {
-        public static final int ProgressBar=0x7f080003;
-        public static final int action_example=0x7f080008;
-        public static final int action_settings=0x7f080007;
-        public static final int container=0x7f080001;
-        public static final int drawer_layout=0x7f080000;
-        public static final int imageView1=0x7f080004;
-        public static final int navigation_drawer=0x7f080002;
-        public static final int section_label=0x7f080006;
-        public static final int textView1=0x7f080005;
+        public static final int ProgressBar=0x7f090003;
+        public static final int action_example=0x7f09000e;
+        public static final int action_settings=0x7f09000d;
+        public static final int container=0x7f090001;
+        public static final int drawer_layout=0x7f090000;
+        public static final int genre=0x7f09000b;
+        public static final int imageView1=0x7f090004;
+        public static final int listPesquisas=0x7f090007;
+        public static final int navigation_drawer=0x7f090002;
+        public static final int rating=0x7f09000a;
+        public static final int releaseYear=0x7f09000c;
+        public static final int section_label=0x7f090006;
+        public static final int textView1=0x7f090005;
+        public static final int thumbnail=0x7f090008;
+        public static final int title=0x7f090009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_splash_screen=0x7f030001;
         public static final int drawer_list=0x7f030002;
-        public static final int fragment_home=0x7f030003;
-        public static final int fragment_mail=0x7f030004;
-        public static final int fragment_main=0x7f030005;
-        public static final int fragment_navigation_drawer=0x7f030006;
+        public static final int fragment_mail=0x7f030003;
+        public static final int fragment_main=0x7f030004;
+        public static final int fragment_navigation_drawer=0x7f030005;
+        public static final int fragment_principal=0x7f030006;
         public static final int fragment_settings=0x7f030007;
+        public static final int list_row=0x7f030008;
     }
     public static final class menu {
-        public static final int global=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int global=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
-        public static final int action_example=0x7f050009;
-        public static final int action_settings=0x7f05000a;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int message_back_pressed=0x7f05000b;
-        public static final int navigation_drawer_close=0x7f050008;
-        public static final int navigation_drawer_open=0x7f050007;
-        public static final int title_activity_main=0x7f050003;
-        public static final int title_activity_splash_screen=0x7f050001;
-        public static final int title_section1=0x7f050004;
-        public static final int title_section2=0x7f050005;
-        public static final int title_section3=0x7f050006;
+        public static final int action_example=0x7f060009;
+        public static final int action_settings=0x7f06000a;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int message_back_pressed=0x7f06000b;
+        public static final int navigation_drawer_close=0x7f060008;
+        public static final int navigation_drawer_open=0x7f060007;
+        public static final int string_loading=0x7f06000d;
+        public static final int string_pesquisa_imagem=0x7f06000c;
+        public static final int title_activity_main=0x7f060003;
+        public static final int title_activity_splash_screen=0x7f060001;
+        public static final int title_section1=0x7f060004;
+        public static final int title_section2=0x7f060005;
+        public static final int title_section3=0x7f060006;
     }
     public static final class style {
         /** 
@@ -94,10 +147,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
